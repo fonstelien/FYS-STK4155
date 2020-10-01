ed50c3745965d195231120f9978b397a71fc775f
fonstelien <olav.fonstelien@gmail.com>
2020-10-01 12:32:22 +0200

    first commit

---
+++ project1/data/show_tiff.py
@@ -0,0 +1,18 @@
+import numpy as np
+from imageio import imread
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+import sys
+
+file_name = sys.argv[1]
+
+# Load the terrain
+terrain1 = imread(file_name)
+# Show the terrain
+plt.figure()
+plt.title(file_name)
+plt.imshow(terrain1, cmap='gray')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.show()
