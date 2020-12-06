e54be31fa7247d2d8656bb9d7f83e20d1281d8ff
fonstelien <olav.fonstelien@gmail.com>
2020-12-06 12:34:52 +0100

    first commit

---
+++ project3/source/autoregression.py
@@ -0,0 +1,59 @@
+'''Methods for autoregression with exogenous signals (ARX modelling).'''
+
+import utils
+
+
+def shifts(df, shift_max):
+    '''Adds to df its first column shifted by 1,2,...,shift_max forwards in time. The new columns are named "<first_col_name><shift>". Returns the modified pd.DataFrame.'''
+    result_df = df.copy()
+    col_name = df.columns[-1]
+
+    for shift in range(1,shift_max+1):
+        result_df[col_name + str(shift)] = df.shift(periods=shift)
+
+    return result_df
+
+
+def make_design_matrix(aux_df, current_df, temp_df, shift_max):
+    '''Makes design matrix from the exogenous signals in aux_df and current_df and the target temp_df, with shift_max shifts in the temperatures. All rows containing np.NaN are dropped from the design matrix. Returns pd.DataFrames with design matrix and aligned targets (X, y).'''
+    X = aux_df.copy()
+    X = X.join(current_df, how='outer')
+    X = X.join(shifts(temp_df, shift_max), how='outer')
+    X = X.dropna()
+    
+    y = X[temp_df.columns]
+    X = X.drop(columns=temp_df.columns)
+
+    return X, y
+
+
+def ols(X, y, start=None, end=None):
+    '''Performs Ordinary Least Squares fitting of design matrix X to targets y in time delta (start,end). Returns np.ndarray with coefficient estimates. '''
+    X = X[start:end]
+    y = y[start:end]
+    coeffs = np.linalg.pinv(X) @ y
+    return coeffs.to_numpy()
+
+
+def evaluate(coeffs, X, init_temp, start=None, end=None):
+    '''Evaluates model defined by the coeffs np.ndarray and the exogenous signals in X, the design matrix pd.DataFrame, with the initial temperature init_temp. Optional limitation to (start,end) time delta. Returns predicted temperature time series as pd.DataFrame.'''
+    X = X[start:end]
+    p = len(coeffs)
+    t_mem = len(X.filter(regex='temp*').columns)
+
+    temp_df = X.copy()
+    temp_df = temp_df.drop(columns=X.columns)
+    temp_df['temp'] = init_temp
+
+    X = X.copy()
+    X.iloc[:,-1*t_mem:] = init_temp
+
+    for i in range(1, temp_df.size):
+        k = 0
+        for j in range(p-1, p-t_mem, -1):
+            k = j
+            X.iloc[i,j] = X.iloc[i-1,j-1]
+        X.iloc[i,k-1] = temp_df.iloc[i-1,0]
+        temp_df.iloc[i,0] = X.iloc[i] @ coeffs
+        
+    return temp_df
---
+++ project3/source/utils.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+import sklearn as skl
+
+
+def parse_file(fname, col_name=None):
+    '''Parses fname csv formatted file. "Stamp" parsed as np.datetime64[m] objects and set as index in returned pd.DataFrame. Time sorted in ascending order. Any duplicates are dropped. Optional rename of "Value" to col_name.'''
+    df = pd.read_csv(fname, parse_dates=['Stamp'], index_col=0)
+    df = df.sort_index()
+    df.index = df.index.values.astype('datetime64[m]')
+    df = df[~df.index.duplicated(keep='first')]
+    if col_name:
+        df = df.rename(columns={'Value':col_name})
+    return df
+
+
+def scale(df, **kwargs):
+    '''Wrapper for skl.preprocessing.StandardScaler. kwargs are forwarded. Does fit_transform() on all columns in df.'''
+    scaler = skl.preprocessing.StandardScaler(**kwargs)
+    for col in df.columns:
+        feature = df[col].to_numpy()
+        feature = feature.reshape(-1,1)
+        feature = scaler.fit_transform(feature)
+        df[col] = feature
+
+    return df
+
+def sample_gaps(df):
+    '''Finds all gaps > 25 minutes between two consequtive samples and returns them in a np.DataFrame.'''
+    T = pd.DataFrame()
+    T['time'] = df.index[1:]
+    T['gap'] = df.index[1:] - df.index[:-1]
+    T = T.set_index('time')
+    T = T.asfreq(freq='T', method='bfill')
+    T = T.asfreq(freq='10T')
+    T = T[T['gap'] > np.timedelta64(25, 'm')]
+    T['gap'] = 1
+    return T
+
+# def fill_gaps(df):
+#     df = df.asfreq(freq='T', method='ffill')
+#     return df.asfreq(freq='10T')
