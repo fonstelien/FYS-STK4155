1975346bb8d88aa53f36401362803c2247256ce3
fonstelien <olav.fonstelien@gmail.com>
2020-12-07 15:03:15 +0100

    first commit

---
+++ project3/source/neural_network.py
@@ -0,0 +1,16 @@
+import utils
+import numpy as np
+import pandas as pd
+
+
+def make_design_matrix(aux_df, current_df, temp_df):
+    ''''''
+    X = aux_df.copy()
+    X = X.join(current_df, how='outer')  # Loading
+    X = X.join(temp_df, how='outer')
+    X = X.dropna()
+    
+    y = X[temp_df.columns]
+    X = X.drop(columns=temp_df.columns)
+
+    return X, y
